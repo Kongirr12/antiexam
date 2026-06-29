4c863cfa8732ffc0bf742008e4afb40ba5b0d462
Kongirr12 <kongirr.12@gmail.com>
2026-06-29 23:51:09 +0700

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -6,7 +6,7 @@ window.QuestionsModule = {
         questions: [],
         isLoading: true
     },
- 
+  
     async render() {
         App.container.innerHTML = `
             <div class="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
